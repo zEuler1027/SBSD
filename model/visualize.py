4d7163b0440d337598e847916f178bf2d7663566
songze huo <1366808715@qq.com>
2024-06-05 00:39:17 +0800

    add amination

--- model/visualize.py
+++ model/visualize.py
@@ -1,11 +1,21 @@
 import py3Dmol
+from pymatgen.core.structure import Molecule 
 
 
-def draw_mol(file_path):
+def draw_mol_xyz(file_path):
     with open(file_path, 'r') as fo:
         xyz = fo.read()
     view = py3Dmol.view(width=800, height=400)
     view.addModel(xyz, 'xyz')
+    view.setStyle({'sphere': {'scale': 0.35}, 'stick': {'radius': 0.20}})
+    view.zoomTo()
+    view.show()
+    
+    
+def draw_mol_pymatgen(mol: Molecule):
+    xyz_str = mol.to(fmt="xyz")
+    view = py3Dmol.view(width=800, height=400)
+    view.addModel(xyz_str, 'xyz')
     view.setStyle({'sphere': {'scale': 0.35}, 'stick': {'radius': 0.20}})
     view.zoomTo()
     view.show()
